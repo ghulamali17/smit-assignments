cc551cc4d2ef1880dfc98dafbbc81e186b7bb740
ghulamali17 <ghulamali47092@gmail.com>
2024-08-29 11:34:09 -0700

    js assignments added

---
+++ js assignments/app.js
@@ -0,0 +1,88 @@
+function show(){
+    alert("Hello User")
+}
+function passErr(){
+    alert("Error! Please enter a valid password")
+}
+function lineBreak(){
+    alert("Welcome to JS Land... \n Happy Coding")
+}
+function sequence(){
+    alert("Welcome to JS Land...")
+    alert(" Happy Coding \n Prevant this page from creating addition dialogs ")
+}
+  let userName;
+
+  let myName="Ghulam Ali"
+
+ function showMsg(){
+    let message;
+    message="Hello world"
+    alert(message)
+ }
+ function bioData(){
+    let name="Jhon Doe";
+    let age=15;
+    certificate=" Certified Mobile Application Development"
+    alert(name)
+    alert(age + " years old")
+    alert(certificate)
+ }
+ function pizza(){
+    let pizza="PIZZA\nPIZZ\nPIZ\nPI\nP"
+    alert(pizza)
+ }
+
+ function email(){
+   let email="ali@gmail.com"
+   alert("My email address is " + email)
+ }
+ function book(){
+    let book="A smarter way to learn JavaScipt"
+    alert("I am try to learn from the Book " + book)
+ }
+ function text(){
+   document.write("Yah! I can write HTML content through JavaScript")
+ }
+ function decString(){
+    let stylishString = "═════ஜ۩۞۩ஜ═════";
+    alert(stylishString)
+ }
+
+ let age=23;
+
+ function visit(){
+    let visit=20;
+    alert(`You have visited this website ${visit} times `)
+ }
+ function birthYear(){
+    let birthYear=2000;
+    document.write(`My birth year is ${birthYear} \n 
+        Data type of my declared variable is ${typeof birthYear}.`)
+ }
+ function clothingStore(){
+    name= "Jhon Doe";
+    title="T-shirt(s)"
+    quantity=5;
+    document.write(` ${name} 
+       ordered ${quantity} ${title} on XYZ Clothing store.`)
+ }
+ function variabless(){
+ // 3 variables in one statement.
+ var firstVar, secondVar, thirdVar;
+
+ // Legal variable names:
+ var myVar;
+ var _myVar;
+ var $myVar;
+ var myVar2;
+ var my_var;
+
+//  Illegal variable names 
+//  var 2myVar;   
+//  var my-var;    
+//  var my var;   
+//  var @myVar;    
+//  var var;  
+
+ }
